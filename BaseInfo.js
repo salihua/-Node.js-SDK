e201eafb23210b89f2297863c026fd3890810f74
追梦的孩子 <40930137+salihua@users.noreply.github.com>
2020-07-06 16:33:57 +0800

    Update BaseInfo.js

--- BaseInfo.js
+++ BaseInfo.js
@@ -4,9 +4,9 @@
 module.exports =
 {
     methods: ['password'],
-    username: 'hw34506969',
-    password: '19981031LIHUACC?',
-    domain: 'hw34506969',
-    projectId:'087c301e2b0025292f4ec001de5285d2',
-    productId:'5eead5d1d88c3107c9e99b50'
+    username: '',
+    password: '',
+    domain: '',
+    projectId:'',
+    productId:''
 }
